4de6e0cbe0e023f06e0ed7f32338b96877b883b0
Titus Tyler Chima <98037951+sycancodes@users.noreply.github.com>
2022-01-31 12:45:45 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,127 @@
+
+// BUDGET CONTROLLER
+var budgetController = (function(){
+    
+})
+
+
+
+//UI CONTROLLER
+var UIController = (function() {
+    
+    //Some Code
+    
+})();
+
+
+
+
+//GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+    
+    document.querySelector('.add__btn').addEventListener('click', function() {
+        
+        //1. Get the field input data
+        
+        //2. Add the item to the budget controller
+        
+        //3. Add the item to the UI
+        
+        //4. Calculate the budget
+        
+        //5. Display the budget on the UI
+    });
+    
+    document.addEventListener('kepress', function(event) {
+        
+        console.log(event);
+    });
+    
+    
+})(budgetController, UIController);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
