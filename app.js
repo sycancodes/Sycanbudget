8c68704b9724a3d11258b1bc8d00c1c6a1ae15a8
Titus Tyler Chima <98037951+sycancodes@users.noreply.github.com>
2022-02-02 11:56:33 +0000

    Reading input data from the UI

--- app.js
+++ app.js
@@ -1,43 +1,72 @@
 
 // BUDGET CONTROLLER
-const budgetController = (function(){
+const budgetController = (function () {
     //Some code here
-    
+
 })
 
 
 
 //UI CONTROLLER
-const UIController = (function() {
-    
-    //Some Code
-    
+const UIController = (function () {
+
+    const DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+    return {
+        getInput: function () {
+
+            return {
+                type : document.querySelector(DOMstrings.inputType).value,
+                description : document.querySelector(DOMstrings.inputDescription).value,
+                value : document.querySelector(DOMstrings.inputValue).value
+
+            };
+            
+        },
+
+        getDOMstrings : function () {
+            return DOMstrings;
+        }
+    };
+
 })();
 
 
 
 
 //GLOBAL APP CONTROLLER
-const controller = (function(budgetCtrl, UICtrl) {
+const controller = (function (budgetCtrl, UICtrl) {
 
-    const ctrlAddItem = function(){
-      //1. Get the field input data
-      //2. Add the item to the budget controller
-      //3. Add the item to the UI
-      //4. Calculate the budget
-      //5. Display the budget on the UI
-      console.log('Clicked');
-    }
-    
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    const DOM = UICtrl.getDOMstrings();
+
+    const ctrlAddItem = function () {
+        //1. Get the field input data
+        const input = UICtrl.getInput();
+        console.log(input)
+        //2. Add the item to the budget controller
+        //3. Add the item to the UI
+        //4. Calculate the budget
+        //5. Display the budget on the UI
         
-    document.addEventListener("kepress", function (event) {
-       if (event.keyCode === 13 || event.which === 13) {
-           ctrlAddItem();
-       }
-     });
+    }
 
-    }) (budgetController, UIController);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener("kepress", function (event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+
+
+
+    });
+
+})(budgetController, UIController);
 
 
 
